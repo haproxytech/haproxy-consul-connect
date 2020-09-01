haproxytech/haproxy-consul-connect

commit 86295ebebf516456ff2b006f091c188f5d7c9adb

BUG/MEDIUM: sort states before comparing them, improve apply loop
* Fix: sort states before comparing them

* Rework haproxy apply loop

* The sync config code path was broken in some cases
* Logs were missleading in some cases
* Use only one goroutine for everything, getting rid of atomic
synchronization and making things simpler

haproxy/state.go
@@ -1,7 +1,6 @@
 package haproxy
 
 import (
-	"sync/atomic"
 	"time"
 
 	"github.com/haproxytech/haproxy-consul-connect/consul"
@@ -11,129 +10,118 @@ import (
 	"gopkg.in/d4l3k/messagediff.v1"
 )
 
+const (
+	stateApplyThrottle   = 500 * time.Millisecond
+	resyncConfigInterval = 5 * time.Minute
+	retryBackoff         = 3 * time.Second
+)
+
 func (h *HAProxy) watch(sd *lib.Shutdown) error {
-	throttle := time.Tick(50 * time.Millisecond)
-	currentState := state.State{}
-	nextState := &atomic.Value{}
-	next := make(chan struct{}, 1)
+	throttle := time.Tick(stateApplyThrottle)
+	resyncConfig := time.Tick(resyncConfigInterval)
+	retry := make(chan struct{})
+
+	var currentState state.State
+	var currentConfig consul.Config
 	dirty := false
+	started := false
 
-	go func() {
-		for c := range h.cfgC {
-			select {
-			case <-sd.Stop:
-				return
-			default:
-			}
-
-			log.Info("received consul config update")
-			nextState.Store(c)
-			h.currentConsulConfig = &c
-			select {
-			case next <- struct{}{}:
-			default:
-			}
-		}
-	}()
-
-	go func() {
-		for range time.Tick(5 * time.Minute) {
-			select {
-			case <-sd.Stop:
-				return
-			default:
-			}
-
-			dirty = true
-		}
-	}()
-
-	retry := func() {
-		time.Sleep(3 * time.Second)
+	waitAndRetry := func() {
+		time.Sleep(retryBackoff)
 		select {
-		case next <- struct{}{}:
+		case retry <- struct{}{}:
 		default:
 		}
 	}
 
-	started := false
 	for {
+		inputReceived := false
+	Throttle:
 		for {
 			select {
 			case <-sd.Stop:
 				return nil
-			case <-next:
-			}
 
-			<-throttle
-
-			log.Info("handling new configuration")
-			if !started {
-				err := h.start(sd)
-				if err != nil {
-					return err
+			case <-throttle:
+				if inputReceived {
+					break Throttle
 				}
-				started = true
-				close(h.Ready)
+
+			case c := <-h.cfgC:
+				log.Info("handling new configuration")
+				currentConfig = c
+				inputReceived = true
+			case <-resyncConfig:
+				log.Info("periodic haproxy config sync check")
+				dirty = true
+				inputReceived = true
+			case <-retry:
+				log.Warn("retrying to apply config")
+				dirty = true
+				inputReceived = true
 			}
-
-			if dirty {
-				log.Info("refreshing haproxy state")
-				fromHa, err := state.FromHAProxy(h.dataplaneClient)
-				if err != nil {
-					log.Errorf("error retrieving haproxy conf: %s", err)
-					retry()
-					continue
-				}
-				diff, equal := messagediff.PrettyDiff(currentState, fromHa)
-				if !equal {
-					log.Errorf("diff found between expected state and haproxy state: %s", diff)
-				}
-				currentState = fromHa
-				dirty = false
-			}
-
-			newConsulCfg := nextState.Load().(consul.Config)
-
-			newState, err := state.Generate(state.Options{
-				EnableIntentions: h.opts.EnableIntentions,
-				LogRequests:      h.opts.LogRequests,
-				LogSocket:        h.haConfig.LogsSock,
-				SPOEConfigPath:   h.haConfig.SPOE,
-				SPOESocket:       h.haConfig.SPOESock,
-			}, h.haConfig, currentState, newConsulCfg)
-			if err != nil {
-				log.Error(err)
-				retry()
-				continue
-			}
-
-			if currentState.Equal(newState) {
-				log.Info("no change to apply to haproxy")
-				continue
-			}
-
-			tx := h.dataplaneClient.Tnx()
-
-			log.Debugf("applying new state: %+v", newState)
-
-			err = state.Apply(tx, currentState, newState)
-			if err != nil {
-				log.Error(err)
-				retry()
-				continue
-			}
-
-			err = tx.Commit()
-			if err != nil {
-				log.Error(err)
-				retry()
-				continue
-			}
-
-			currentState = newState
-
-			log.Info("state applied")
 		}
+
+		if !started {
+			err := h.start(sd)
+			if err != nil {
+				return err
+			}
+			started = true
+			close(h.Ready)
+		}
+
+		if dirty {
+			fromHa, err := state.FromHAProxy(h.dataplaneClient)
+			if err != nil {
+				log.Errorf("error retrieving haproxy conf: %s", err)
+				waitAndRetry()
+				continue
+			}
+			diff, equal := messagediff.PrettyDiff(currentState, fromHa)
+			if !equal {
+				log.Errorf("diff found between expected state and haproxy state: %s", diff)
+			}
+			currentState = fromHa
+			dirty = false
+		}
+
+		newState, err := state.Generate(state.Options{
+			EnableIntentions: h.opts.EnableIntentions,
+			LogRequests:      h.opts.LogRequests,
+			LogSocket:        h.haConfig.LogsSock,
+			SPOEConfigPath:   h.haConfig.SPOE,
+			SPOESocket:       h.haConfig.SPOESock,
+		}, h.haConfig, currentState, currentConfig)
+		if err != nil {
+			log.Error(err)
+			continue
+		}
+
+		if currentState.Equal(newState) {
+			log.Info("no change to apply to haproxy")
+			continue
+		}
+
+		tx := h.dataplaneClient.Tnx()
+
+		log.Debugf("applying new state: %+v", newState)
+
+		err = state.Apply(tx, currentState, newState)
+		if err != nil {
+			log.Error(err)
+			waitAndRetry()
+			continue
+		}
+
+		err = tx.Commit()
+		if err != nil {
+			log.Error(err)
+			waitAndRetry()
+			continue
+		}
+
+		currentState = newState
+		log.Info("state applied")
 	}
 }

haproxy/state/from_ha.go
@@ -2,6 +2,7 @@ package state
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/haproxytech/models"
 )
@@ -77,6 +78,8 @@ func FromHAProxy(ha HAProxyRead) (State, error) {
 		})
 	}
 
+	sort.Sort(Frontends(state.Frontends))
+
 	haBackends, err := ha.Backends()
 	if err != nil {
 		return state, err
@@ -116,6 +119,8 @@ func FromHAProxy(ha HAProxyRead) (State, error) {
 			HTTPRequestRules: reqRules,
 		})
 	}
+
+	sort.Sort(Backends(state.Backends))
 
 	return state, nil
 }

haproxy/state/from_ha_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"sort"
 	"testing"
 
 	"github.com/haproxytech/haproxy-consul-connect/haproxy/haproxy_cmd"
@@ -128,13 +127,6 @@ RHmDi0qnL6qrKfjTOnfHgQPCgxAy9knMIiDzBRg=
 	require.NoError(t, err)
 	require.Equal(t, len(state.Backends), len(current.Backends))
 	require.Equal(t, len(state.Frontends), len(current.Frontends))
-
-	// Sort to be sure order is predictible
-	sort.Sort(Frontends(state.Frontends))
-	sort.Sort(Frontends(current.Frontends))
-	// Sort to be sure order is predictible
-	sort.Sort(Backends(state.Backends))
-	sort.Sort(Backends(current.Backends))
 
 	require.Equal(t, state.Backends, current.Backends)
 	require.Equal(t, state.Frontends, current.Frontends)

haproxy/state/states.go
@@ -3,7 +3,6 @@ package state
 import (
 	"fmt"
 	"sort"
-	"strings"
 
 	"github.com/haproxytech/haproxy-consul-connect/consul"
 	"github.com/haproxytech/models"
@@ -70,13 +69,8 @@ func Generate(opts Options, certStore CertificateStore, oldState State, cfg cons
 		}
 	}
 
-	sort.Slice(newState.Frontends, func(i, j int) bool {
-		return strings.Compare(newState.Frontends[i].Frontend.Name, newState.Frontends[j].Frontend.Name) < 0
-	})
-
-	sort.Slice(newState.Backends, func(i, j int) bool {
-		return strings.Compare(newState.Backends[i].Backend.Name, newState.Backends[j].Backend.Name) < 0
-	})
+	sort.Sort(Frontends(newState.Frontends))
+	sort.Sort(Backends(newState.Backends))
 
 	return newState, nil
 }

commit 19573c84daf66c13c1811e151fc8df02506d3e6f

BUG/MEDIUM: crash on intentions check (#68)
This was introduced by 86295ebebf516456ff2b006f091c188f5d7c9adb

haproxy/state.go
@@ -49,6 +49,7 @@ func (h *HAProxy) watch(sd *lib.Shutdown) error {
 
 			case c := <-h.cfgC:
 				log.Info("handling new configuration")
+				h.currentConsulConfig = &c
 				currentConfig = c
 				inputReceived = true
 			case <-resyncConfig: