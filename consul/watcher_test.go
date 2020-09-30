d41be6f01ff42eec69290219a23f221d2d3ffefb
Florian Domain <f.domain@criteo.com>
2020-09-30 17:40:47 +0200

    BUG/MINOR: update haproxy config when upstreams config is updated
    When upstreams configuration is changed in Consul, this is not
    take into account by consul-connect until a restart.

--- consul/watcher_test.go
+++ consul/watcher_test.go
@@ -29,6 +29,17 @@ func startAgent(t *testing.T, sd *lib.Shutdown) *api.Client {
 	return client
 }
 
+func clearCerts(cfg *Config) {
+	cfg.Downstream.CAs = nil
+	cfg.Downstream.Cert = nil
+	cfg.Downstream.Key = nil
+	for i := range cfg.Upstreams {
+		cfg.Upstreams[i].CAs = nil
+		cfg.Upstreams[i].Cert = nil
+		cfg.Upstreams[i].Key = nil
+	}
+}
+
 var testCases = []struct {
 	name     string
 	reg      *api.AgentServiceRegistration
@@ -207,7 +218,7 @@ var testCases = []struct {
 	},
 }
 
-func TestWatcher(t *testing.T) {
+func TestWatcherConfigInit(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			sd := lib.NewShutdown()
@@ -215,7 +226,20 @@ func TestWatcher(t *testing.T) {
 
 			consul := startAgent(t, sd)
 
-			err := consul.Agent().ServiceRegister(tc.reg)
+			_, _, err := consul.PreparedQuery().Create(&api.PreparedQueryDefinition{
+				Name: "pq-",
+				Service: api.ServiceQuery{
+					Service:     "${match(1)}",
+					OnlyPassing: true,
+				},
+				Template: api.QueryTemplate{
+					Type:   "name_prefix_match",
+					Regexp: "^pq-(.+)$",
+				},
+			}, &api.WriteOptions{})
+			require.NoError(t, err)
+
+			err = consul.Agent().ServiceRegister(tc.reg)
 			require.NoError(t, err)
 
 			w := New(tc.reg.ID, consul, log.New())
@@ -232,19 +256,159 @@ func TestWatcher(t *testing.T) {
 			case err := <-errs:
 				require.NoError(t, err)
 			case cfg := <-w.C:
-				// clear Certs
-				cfg.Downstream.CAs = nil
-				cfg.Downstream.Cert = nil
-				cfg.Downstream.Key = nil
-				for i := range cfg.Upstreams {
-					cfg.Upstreams[i].CAs = nil
-					cfg.Upstreams[i].Cert = nil
-					cfg.Upstreams[i].Key = nil
-				}
-
+				clearCerts(&cfg)
 				require.Equal(t, tc.expected, cfg)
 			}
 		})
 	}
 
+}
+
+func TestWatcherConfigUpdate(t *testing.T) {
+	sd := lib.NewShutdown()
+	defer sd.Shutdown("test end")
+
+	consul := startAgent(t, sd)
+
+	err := consul.Agent().ServiceRegister(
+		&api.AgentServiceRegistration{
+			Name: "server",
+		},
+	)
+	require.NoError(t, err)
+
+	_, _, err = consul.PreparedQuery().Create(&api.PreparedQueryDefinition{
+		Name: "pq-",
+		Service: api.ServiceQuery{
+			Service:     "${match(1)}",
+			OnlyPassing: true,
+		},
+		Template: api.QueryTemplate{
+			Type:   "name_prefix_match",
+			Regexp: "^pq-(.+)$",
+		},
+	}, &api.WriteOptions{})
+	require.NoError(t, err)
+
+	err = consul.Agent().ServiceRegister(
+		&api.AgentServiceRegistration{
+			Name: "client",
+			ID:   "client-inst",
+			Port: 8080,
+			Connect: &api.AgentServiceConnect{
+				SidecarService: &api.AgentServiceRegistration{
+					Proxy: &api.AgentServiceConnectProxyConfig{
+						Upstreams: []api.Upstream{
+							{
+								DestinationType: "service",
+								DestinationName: "server",
+								LocalBindPort:   8081,
+								Config: map[string]interface{}{
+									"connect_timeout": "1m",
+									"read_timeout":    "1m",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	)
+	require.NoError(t, err)
+
+	w := New("client-inst", consul, log.New())
+
+	errs := make(chan error)
+	go func() {
+		err := w.Run()
+		if err != nil {
+			errs <- err
+		}
+	}()
+
+	expected := Config{
+		ServiceName: "client",
+		ServiceID:   "client-inst",
+		Downstream: Downstream{
+			LocalBindAddress: "0.0.0.0",
+			LocalBindPort:    21000,
+			TargetAddress:    "127.0.0.1",
+			TargetPort:       8080,
+			ConnectTimeout:   DefaultConnectTimeout,
+			ReadTimeout:      DefaultReadTimeout,
+		},
+		Upstreams: []Upstream{
+			{
+				Name:             "service_server",
+				LocalBindAddress: "127.0.0.1",
+				LocalBindPort:    8081,
+				ConnectTimeout:   1 * time.Minute,
+				ReadTimeout:      1 * time.Minute,
+			},
+		},
+	}
+
+	select {
+	case err := <-errs:
+		require.NoError(t, err)
+	case cfg := <-w.C:
+		clearCerts(&cfg)
+		require.Equal(t, expected, cfg)
+	}
+
+	err = consul.Agent().ServiceRegister(
+		&api.AgentServiceRegistration{
+			Name: "client",
+			ID:   "client-inst",
+			Port: 8080,
+			Connect: &api.AgentServiceConnect{
+				SidecarService: &api.AgentServiceRegistration{
+					Proxy: &api.AgentServiceConnectProxyConfig{
+						Upstreams: []api.Upstream{
+							{
+								DestinationType: "service",
+								DestinationName: "server",
+								LocalBindPort:   8082,
+								Config: map[string]interface{}{
+									"connect_timeout": "2m",
+									"read_timeout":    "2m",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	)
+	require.NoError(t, err)
+
+	expected = Config{
+		ServiceName: "client",
+		ServiceID:   "client-inst",
+		Downstream: Downstream{
+			LocalBindAddress: "0.0.0.0",
+			LocalBindPort:    21000,
+			TargetAddress:    "127.0.0.1",
+			TargetPort:       8080,
+			ConnectTimeout:   DefaultConnectTimeout,
+			ReadTimeout:      DefaultReadTimeout,
+		},
+		Upstreams: []Upstream{
+			{
+				Name:             "service_server",
+				LocalBindAddress: "127.0.0.1",
+				LocalBindPort:    8082,
+				ConnectTimeout:   2 * time.Minute,
+				ReadTimeout:      2 * time.Minute,
+			},
+		},
+	}
+
+	select {
+	case err := <-errs:
+		require.NoError(t, err)
+	case cfg := <-w.C:
+		clearCerts(&cfg)
+		require.Equal(t, expected, cfg)
+	}
 }
