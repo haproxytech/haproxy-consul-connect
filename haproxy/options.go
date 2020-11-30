97e02d84f9c984e22ad74c8d42a0e91f28ec3854
Mathilde Gilles <ma.gilles@criteo.com>
2020-11-30 21:46:49 +0100

    Allow to pass haproxy defaults and global parameters as flag
    To give better control to users in fine tuning haproxy when using
    connect, give the ability to pass command line flags to modify haproxy's
    configuration.
    The flags are in the form {type}.{name}={value} and can be repeated.
    Examples:
    * -haproxy-param "global.tune.bufsize=33792"
    * -haproxy-param "default.retries=1"

--- haproxy/options.go
+++ haproxy/options.go
@@ -1,5 +1,30 @@
 package haproxy
 
+type HAProxyParams struct {
+	Defaults map[string]string
+	Globals  map[string]string
+}
+
+func (p HAProxyParams) With(other HAProxyParams) HAProxyParams {
+	new := HAProxyParams{
+		Defaults: map[string]string{},
+		Globals:  map[string]string{},
+	}
+	for k, v := range p.Defaults {
+		new.Defaults[k] = v
+	}
+	for k, v := range other.Defaults {
+		new.Defaults[k] = v
+	}
+	for k, v := range p.Globals {
+		new.Globals[k] = v
+	}
+	for k, v := range other.Globals {
+		new.Globals[k] = v
+	}
+	return new
+}
+
 type Options struct {
 	HAProxyBin           string
 	DataplaneBin         string
@@ -9,4 +34,5 @@ type Options struct {
 	StatsListenAddr      string
 	StatsRegisterService bool
 	LogRequests          bool
+	HAProxyParams        HAProxyParams
 }
